0c18d091c71d667a3c36229ff34fee8441e3c30f
blaurensayshi <39201295+blaurensayshi@users.noreply.github.com>
2018-06-09 19:12:01 -0300

    Update Exercício 4.py

--- Aula1/Exercício 4.py
+++ Aula1/Exercício 4.py
@@ -6,8 +6,8 @@ def horas_segundos(h,m,s,x):
     return h*x*x+m*x+s
 
 x = int(60)
-h = int(input("Digite as horas"))
-m = int(input("Digite os minutos"))
-s = int(input("Digite os segundos"))
+h = int(input("Digite as horas: "))
+m = int(input("Digite os minutos:"))
+s = int(input("Digite os segundos: "))
 
 print("São", horas_segundos(h,m,s,x))
