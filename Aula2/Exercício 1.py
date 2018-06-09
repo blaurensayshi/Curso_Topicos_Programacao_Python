9fe75adf5b221ae9e4cb188da40c0deb6ca68b92
blaurensayshi <39201295+blaurensayshi@users.noreply.github.com>
2018-06-09 19:19:29 -0300

    Update Exercício 1.py

--- Aula2/Exercício 1.py
+++ Aula2/Exercício 1.py
@@ -2,13 +2,13 @@
 Programa soma ou multiplica dois números
 """
 
-a = int(input("Digite um número"))
-b = int(input("Digite outro número"))
-operacao = input("Soma ou Multiplicação?")
+a = int(input("Digite um número: "))
+b = int(input("Digite outro número: "))
+operacao = input("Soma ou Multiplicação? ")
 soma = a+b;
 mult = a*b;
 
-if operacao == 'soma':
+if operacao == 'soma' or operacao == 'Soma' or operacao == 'SOMA' :
     print(soma)
 else:
     print(mult)
