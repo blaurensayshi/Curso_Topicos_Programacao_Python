4d4afbecb7e35d8d899798c81e1b031ff7453d9e
blaurensayshi <39201295+blaurensayshi@users.noreply.github.com>
2018-06-09 19:23:05 -0300

    Update Exercício 3.py

--- Aula2/Exercício 3.py
+++ Aula2/Exercício 3.py
@@ -2,13 +2,13 @@
 
 """
 
-a = float(input("Qual foi a sua frequência nessa matéria?"))
+a = float(input("Qual foi a sua frequência nessa matéria? "))
 if a < 75:
     print("Reprovado")
 else:
-    print("Digite sua nota na p1")
+    print("Digite sua nota na P1: ")
     p1 = float(input())
-    p2 = float(input("Digite sua nota na P2"))
+    p2 = float(input("Digite sua nota na P2: "))
     media1 = (p1+p2)/2
 if a >= 75 and media1 >= 7:
     print("Aprovado")
@@ -16,7 +16,7 @@ elif a >= 75 and media1 < 3:
     print("Reprovado")
 elif a >= 75 and media1 >= 3 and media1 < 7:
     print("Prova Final")
-    pf = float(input("Digite sua nota na PF"))
+    pf = float(input("Digite sua nota na PF: "))
     media2 = (media1+pf)/2
 if a >=75 and media1>=3 and media1<7 and media2>=5:
     print("Aprovado")
