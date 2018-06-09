9b7b41682326b463c33ea30e37b6fc287e5383df
blaurensayshi <39201295+blaurensayshi@users.noreply.github.com>
2018-06-09 19:15:33 -0300

    Update Exercício 1.py

--- Aula1/Exercício 1.py
+++ Aula1/Exercício 1.py
@@ -2,8 +2,8 @@
 Meus dados
 """
 
-nome = input("Digite o seu nome:")
-altura = input("Digite a sua altura em metros:")
-curso = input("Digite o seu curso:")
+nome = input("Digite o seu nome: ")
+altura = input("Digite a sua altura em metros: ")
+curso = input("Digite o seu curso: ")
 
-print("Seu nome é\n", (nome),", sua altura em metros é de\n", (altura), "e vc cursa", (curso))
+print("Seu nome é", (nome),", sua altura em metros é de", (altura), "e vc cursa", (curso))
